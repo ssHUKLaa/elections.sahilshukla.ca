0249bd704c17dba9dfcde851ecc1ca53cb229a7f
shukla <destroyernv2004@gmail.com>
2025-03-26 22:51:13 -0400

    Update landingpage.py

--- landingpage.py
+++ landingpage.py
@@ -135,10 +135,10 @@ def home():
     ''')
 
 @server.route('/redirect-to-can2025')
-def redirect_to_dash():
+def redirect_to_can2025():
     return redirect('/can2025')
 
 @server.route('/redirect-to-us2024')
-def redirect_to_dash():
+def redirect_to_us2024():
     return redirect('/old/us2024')
 
